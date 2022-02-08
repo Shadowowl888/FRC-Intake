1b162928d8a30b30b1c6fb11d115cd5fa6f25f5f
Shadowowl888 <lightningowl2004@gmail.com>
2022-02-08 15:40:51 -0800

    Update Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,15 +1,17 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-#include <frc/Joystick.h>
-#include <frc/TimedRobot.h>
-#include <frc/Timer.h>
-#include <frc/drive/DifferentialDrive.h>
-#include <frc/motorcontrol/PWMSparkMax.h>
-
 #include "Robot.h"
+#include <frc/Joystick.h>
+#include <ctre/Phoenix.h>
+
+TalonSRX Intake = (2);
+frc::Joystick control(0);
+
 void Intake() {
-  if ()
+  if (control.GetRawButtonPressed(5)) {
+    Intake.Set(ControlMode::PercentOutput, -1.0);
+  }
 }
 
 void Robot::RobotInit() {}
@@ -28,7 +30,7 @@ void Robot::TestInit() {}
 void Robot::TestPeriodic() {
   //Put function call in here. Create seperate function outside
   while(true) {
-    Intake()
+    intake();
   }
 }
 
