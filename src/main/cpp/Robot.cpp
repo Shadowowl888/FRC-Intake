586f7da01caaf5fcd5f0bac79198df164ba5f350
Shadowowl888 <lightningowl2004@gmail.com>
2022-02-08 15:24:40 -0800

    Update Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,8 +1,16 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+#include <frc/Joystick.h>
+#include <frc/TimedRobot.h>
+#include <frc/Timer.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/motorcontrol/PWMSparkMax.h>
 
 #include "Robot.h"
+void Intake() {
+  if ()
+}
 
 void Robot::RobotInit() {}
 void Robot::RobotPeriodic() {}
@@ -17,7 +25,12 @@ void Robot::DisabledInit() {}
 void Robot::DisabledPeriodic() {}
 
 void Robot::TestInit() {}
-void Robot::TestPeriodic() {}
+void Robot::TestPeriodic() {
+  //Put function call in here. Create seperate function outside
+  while(true) {
+    Intake()
+  }
+}
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
